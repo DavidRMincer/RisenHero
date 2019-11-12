4aa7807f788b47ac4744f2ae93f6dc4735ab3d0d
DavidRMincer <david.r.mincer@live.com>
2019-11-12 22:03:12 +0000

    Character Prefab

---
+++ RisenHero/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class CharacterBehaviour : MonoBehaviour
+{
+    public float walkSpeed;
+
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Move in direction at speed
+    /// </summary>
+    /// <param name="direction"></param>
+    /// <param name="speed"></param>
+    public void Move(Vector2 direction)
+    {
+        _rb.velocity = direction * walkSpeed * Time.deltaTime;
+    }
+}
---
+++ RisenHero/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : CharacterBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+    }
+}
