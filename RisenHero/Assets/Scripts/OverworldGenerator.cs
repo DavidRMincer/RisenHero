34384033e97f5586e5648cbbe27e914cd0325c21
DavidRMincer <david.r.mincer@live.com>
2019-11-14 00:43:36 +0000

    World Array

--- RisenHero/Assets/Scripts/OverworldGenerator.cs
+++ RisenHero/Assets/Scripts/OverworldGenerator.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class OverworldGenerator : MonoBehaviour
 {
     public Vector2      worldSize;
+    public int          startZone;
     public char         emptyChar = '.',
+                        pathChar = '-',
                         forestChar = 'f',
                         villageChar = 'v',
                         townChar = 't',
@@ -14,18 +16,42 @@ public class OverworldGenerator : MonoBehaviour
                         startChar = 's',
                         endChar = 'x';
 
-    private char[][]    _world;
+    private char[,]    _world;
     private Vector2     _startPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateWorld();
+        Debug.Log(_world);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Procedurally generate overworld
+    /// </summary>
+    public void GenerateWorld()
+    {
+        char[,] data = new char[Mathf.RoundToInt(worldSize.x),Mathf.RoundToInt(worldSize.y)];
+        _world = data;
+
+        // Create blank world
+        for (int xIndex = 0; xIndex < worldSize.x; ++xIndex)
+        {
+            for (int yIndex = 0; yIndex < worldSize.y; ++yIndex)
+            {
+                _world[xIndex, yIndex] = emptyChar;
+            }
+        }
+
+        // Set start point
+        _startPoint.x = Random.Range(0, startZone);
+        _startPoint.y = Random.Range(0, startZone);
+        _world[Mathf.RoundToInt(_startPoint.x), Mathf.RoundToInt(_startPoint.y)] = startChar;
     }
 }
