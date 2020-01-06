fdf3811cbff3c01c69bda8c7e30a8a7caf1238fd
DavidRMincer <david.r.mincer@live.com>
2020-01-06 21:06:09 +0000

    Movement UI tutorial

---
+++ RisenHero/Assets/Scripts/UIManagerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerBehaviour : MonoBehaviour
+{
+    public Image movementInputImg;
+    public float movementTutorialDuration;
+
+    private bool _moved = false;
+    
+    private void LateUpdate()
+    {
+        if (!_moved)
+        {
+            _moved = (Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0);
+        }
+
+        movementInputImg.gameObject.SetActive(Time.time >= movementTutorialDuration && !_moved);
+    }
+}
